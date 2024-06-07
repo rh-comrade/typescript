7102f8755c34defd838f5418473bd2b872f07a7d
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2024-06-07 18:00:31 +0530

    array and methods added

---
+++ variables.js
@@ -0,0 +1,23 @@
+// Its supports all variable of js
+/*
+    String
+    Number
+    Array
+    Object
+    Null
+    Bool
+    any
+*/
+// variables
+var x;
+x = 10;
+console.log(x);
+x = 'it can handle string as well as number data type as mention in its initilization';
+console.log(x);
+// Array
+var arr1 = [1, 2, 3];
+var arra2 = ['a1', 'a2', 'shyam'];
+var arr = [1, 2, 3, 4, { 'a1': 'ram', 'a2': 'shyam' }];
+console.log(arr);
+// all array methods are same as js
+var op = arra2.map(function (item) { return console.log(item); });
